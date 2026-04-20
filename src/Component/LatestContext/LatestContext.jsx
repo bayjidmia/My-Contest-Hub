d484d815b499acf80735e1cf662bc43f76076ed6
bayjidmia <mdbayjidmia982@gmail.com>
2026-04-20 12:58:02 +0600

    resolve loading pb

--- src/Component/LatestContext/LatestContext.jsx
+++ src/Component/LatestContext/LatestContext.jsx
@@ -8,12 +8,14 @@ import { AuthContext } from "../../Authprovide/Context/Context";
 const LatestContext = () => {
   const axiosSecure = useAxiosSecure();
   const { user } = useContext(AuthContext);
-  const { data: contests = [], isLoading } = useQuery({
+  const { data: contests, isLoading } = useQuery({
     queryKey: ["latest-contest"],
     queryFn: async () => {
       const res = await axiosSecure.get("latest-contest");
       return res.data;
     },
+
+    staleTime: 1000 * 60 * 5, // 5 min cache
   });
 
   if (isLoading) {
